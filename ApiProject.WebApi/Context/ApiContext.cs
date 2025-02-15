afea3b1f7970ad6b331110c7f0d72ddfba220ea8
emirhannozmen <emirhannozmen@gmail.com>
2025-02-15 18:47:56 +0300

    ..

--- ApiProject.WebApi/Context/ApiContext.cs
+++ ApiProject.WebApi/Context/ApiContext.cs
@@ -9,15 +9,15 @@ namespace ApiProject.WebApi.Context
         {
             optionsBuilder.UseSqlServer("Server=EMIR;initial catalog=YummyDb;integrated security=true;TrustServerCertificate=True;");
         }
-        DbSet<Category> Categories { get; set; }
-        DbSet<Chef> Chefs { get; set; }
-        DbSet<Contact> Contacts { get; set; }
-        DbSet<Feature> Features { get; set; }
-        DbSet<Image> Images { get; set; }
-        DbSet<Message> Messages { get; set; }
-        DbSet<Product> Products { get; set; }
-        DbSet<Reservation> Reservations { get; set; }
-        DbSet<Service> Services { get; set; }
-        DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
---
+++ ApiProject.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,57 @@
+﻿using ApiProject.WebApi.Context;
+using ApiProject.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ChefList()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok("Şef Listesi");
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return Ok("Şef Ekleme İşlemi Başarılı!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            _context.Chefs.Remove(value);
+            _context.SaveChanges();
+            return Ok("Şef Silme İşlemi Başarılı!");
+        }
+
+        [HttpGet("GetChef")]
+        public IActionResult GetChefById(int id)
+        {
+            var value = _context.Chefs.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef Güncelleme İşlemi Başarılı!");
+        }
+    }
+}
